473f9b27e13614660a1f99a61ac0b0d374cf5de7
Luiz Henrique <luizhenrique.se@gmail.com>
2019-09-11 22:28:32 -0300

    feat: adiciona estrutura inicial do projeto

---
+++ src/main/java/com/meli/simiosapi/SimiosApiApplication.java
@@ -0,0 +1,13 @@
+package com.meli.simiosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiosApiApplication.class, args);
+	}
+
+}
