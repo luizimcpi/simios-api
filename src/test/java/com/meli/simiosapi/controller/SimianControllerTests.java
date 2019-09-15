15fe333c5a68e0899b73323fba8c1c451802c644
Luiz Henrique <luizhenrique.se@gmail.com>
2019-09-15 12:04:31 -0300

    finaliza teste simiancontroller

--- src/test/java/com/meli/simiosapi/controller/SimianControllerTests.java
+++ src/test/java/com/meli/simiosapi/controller/SimianControllerTests.java
@@ -1,6 +1,7 @@
 package com.meli.simiosapi.controller;
 
 import com.meli.simiosapi.contracts.request.SimianRequest;
+import com.meli.simiosapi.exception.BadRequestException;
 import com.meli.simiosapi.service.HistoricService;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,8 +20,12 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class SimianControllerTests {
 
-    private static final List<String> VALID_DNA = Arrays.asList("CTGAGA", "CTATGC", "TATTGT", "AGAGGG", "CCCCTA", "TCACTG");
+    private static final List<String> VALID_SIMIAN_DNA = Arrays.asList("CTGAGA", "CTATGC", "TATTGT", "AGAGGG", "CCCCTA", "TCACTG");
+    private static final List<String> VALID_HUMAN_DNA = Arrays.asList("CTGAGA", "CTATGC", "TAATGT", "AGAGAG", "CTCCTA", "TCACTG");
+    private static final List<String> INVALID_DNA = Arrays.asList("CTGAGA", "CTATGC", "TAATGT", "AGAGAG", "CTCCTA");
     private SimianRequest VALID_SIMIAN_REQUEST = null;
+    private SimianRequest VALID_HUMAN_REQUEST = null;
+    private SimianRequest INVALID_REQUEST = null;
 
     @Mock
     private HistoricService service;
@@ -31,13 +36,43 @@ public class SimianControllerTests {
     @Before
     public void setUp(){
         VALID_SIMIAN_REQUEST = SimianRequest.builder()
-                .dna(VALID_DNA)
+                .dna(VALID_SIMIAN_DNA)
+                .build();
+
+        VALID_HUMAN_REQUEST = SimianRequest.builder()
+                .dna(VALID_HUMAN_DNA)
+                .build();
+
+        INVALID_REQUEST = SimianRequest.builder()
+                .dna(INVALID_DNA)
                 .build();
     }
 
     @Test
     public void deveRetornarStatusConflictQuandoDnaExistirNaBase() {
-        when(service.dnaExists(VALID_DNA.toString())).thenReturn(true);
+        when(service.dnaExists(VALID_SIMIAN_DNA.toString())).thenReturn(true);
         Assert.assertEquals(HttpStatus.CONFLICT, simianController.create(VALID_SIMIAN_REQUEST).getStatusCode());
+    }
+
+
+    @Test
+    public void deveRetornarStatusForbiddenQuandoDnaForHumanoEValido() {
+        when(service.dnaExists(VALID_HUMAN_DNA.toString())).thenReturn(false);
+        when(service.isSimian(VALID_HUMAN_DNA)).thenReturn(false);
+        Assert.assertEquals(HttpStatus.FORBIDDEN, simianController.create(VALID_HUMAN_REQUEST).getStatusCode());
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void deveRetornarErroQuandoDnaForInvalido() {
+        when(service.dnaExists(INVALID_DNA.toString())).thenReturn(false);
+        when(service.isSimian(INVALID_DNA)).thenThrow(BadRequestException.class);
+        simianController.create(INVALID_REQUEST);
+    }
+
+    @Test
+    public void deveRetornarStatusOkQuandoDnaForSimianEValido() {
+        when(service.dnaExists(VALID_SIMIAN_DNA.toString())).thenReturn(false);
+        when(service.isSimian(VALID_SIMIAN_DNA)).thenReturn(true);
+        Assert.assertEquals(HttpStatus.OK, simianController.create(VALID_SIMIAN_REQUEST).getStatusCode());
     }
 }
